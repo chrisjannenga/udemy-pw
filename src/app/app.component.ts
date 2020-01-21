8383f0be52ce2e91ac20dbdc87cab72919fbed15
Chris Jannenga <cjannenga312@gmail.com>
2020-01-20 21:36:33 -0700

    Added styling and pw generation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(value: string) {
+    // tslint:disable-next-line:radix
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+
+    console.log(this.length);
+
+  }
 
   onButtonClick() {
-    this.password = 'My Password!';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
   }
 }
